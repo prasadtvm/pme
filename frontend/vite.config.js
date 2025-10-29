8f6ae2db8082e57ff40ed8152943b62505bbaf94
Prasad <prasadnarayan@gmail.com>
2025-10-29 07:16:31 +0530

    f_vite again updated  2910202507:16am

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],  
+  plugins: [react()], 
+  build: {
+    outDir: 'dist', // tells Vite where to output the production build
+  }, 
   server: {
-    port: 3000    
+    port: 3000, 
+    open: true, // optional: automatically opens browser on 'npm run dev'
+    historyApiFallback: true // ensures React Router paths work in dev
   },
   esbuild: {
     loader: 'jsx',
